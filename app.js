2ee4759734f7922bd27c589c0ee38965655332eb
Andresgueva <46576289+Andresgueva@users.noreply.github.com>
2020-05-29 11:50:07 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-
+/*
+IMPORTANTE INSTALAR
+npm i yargs colors --save
+nmp i csvtojson --save
+IMPORTANTE NO ELIMINAR ISO-3166-ALPHA-3.json DE LA CARPETA MODELO
+CONTIENE LOS CODIGOS DE PAISES NORMA ISO-3166-ALPHA-3 ACTUALES
+FORMA PARA PUBLICAR
+node app.js publicar -f "./nombre_archivo.csv" -c "codigo_pais" -y año
+para visualizar en la web, abra su navegador preferigo e ingrese localhost:3000
+FORMA PARA GUARDAR
+node app.js guardar -f "./nombre_archivo.csv" -c "codigo_pais" -y año -o "nomre_archivo_sin_extension"
+ */
 const argv = require('./config/yargs').argv;
 const buscar = require('./buscador/buscar');
 const colors = require('colors');
